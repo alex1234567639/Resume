a023f584dd0193409ed9e42958f49010ec681417
alex1234567639 <alex8603062000@gmail.com>
2025-03-09 19:29:15 +0800

    [feature]工作經歷頁面

---
+++ src/components/WorkExperience.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="work-experience-container">
+    <Title :title="t('work.title')"/>
+    <Divider :color="'rgba(185,185,185,0.3)'"/>
+
+    <div v-if="Array.isArray(workExperienceList)">
+      <div
+          v-for="(item, index) in workExperienceList"
+          class="work-list-item"
+          :key="index"
+      >
+        <div class="duration">
+          <div class="dot"></div>{{ item.duration }}
+        </div>
+        <div class="title">{{ item.title }}</div>
+        <div class="subtitle">{{ item.subtitle }}</div>
+        <div class="description">{{ item.description }}</div>
+        <div v-for="w in item.website" class="website-item" :key="w.name">
+          <a :href="w.link" target="_blank">- {{ w.name }}</a>
+          <span class="text-[#888888]">{{ `（${w.skills}）` }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Title from "./Title.vue";
+import Divider from "./Divider.vue";
+import { ref } from "vue";
+import { useI18n } from 'vue-i18n'
+const { t, tm } = useI18n()
+
+const workExperienceList = ref(tm("work.list"))
+</script>
+
+<style lang="scss" scoped>
+.work-experience-container {
+  padding: 2vw 5vw;
+  & .work-list-item {
+    padding: 0.7vw 1vw;
+    & .duration {
+      @apply flex items-center text-[1vw] text-[#888888];
+      & .dot {
+        @apply rounded-full bg-[#b4d1eb];
+        width: 0.8vw;
+        height: 0.8vw;
+        margin:  0 0.5vw 0 0;
+      }
+    }
+    & .title {
+      @apply text-[1.3vw] font-bold;
+      margin: 0 0 0 1.2vw;
+    }
+    & .subtitle {
+      @apply text-[1.2vw] text-[#888888];
+      margin: 0 0 0 1.2vw;
+    }
+    & .description {
+      @apply text-[1vw] text-[#888888];
+      margin: 1vw 1.2vw;
+    }
+    & .website-item {
+      @apply text-[1vw] text-[#246bb0];
+      margin: 0.5vw 1.2vw;
+    }
+  }
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -2,12 +2,14 @@
   <div>
     <Info/>
     <Skill/>
+    <WorkExperience/>
     <EducationalQualifications/>
   </div>
 </template>
 
 <script setup>
-import Info from "../components/info.vue";
+import Info from "../components/Info.vue";
 import Skill from "../components/Skill.vue";
+import WorkExperience from "../components/WorkExperience.vue";
 import EducationalQualifications from "../components/EducationalQualifications.vue";
 </script>
