c64f61dd815e99a0d9b2c6434359669c652a1fd1
alex1234567639 <alex8603062000@gmail.com>
2025-03-09 14:01:01 +0800

    [init]

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+    {
+        path: "/",
+        name: "home",
+        component: () => import("../views/index.vue"),
+    },
+    {
+        path: '/:catchAll(.*)',
+        redirect: '/'
+    }
+]
+
+export default createRouter({
+    history: createWebHistory('/home'),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        // always scroll to top
+        return { top: 0 }
+    }
+})
