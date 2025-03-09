439ec13f87e52bb5600078fc9145426b9154d9fa
alex1234567639 <alex8603062000@gmail.com>
2025-03-09 17:02:59 +0800

    [feature]技能頁面

---
+++ src/components/Skill.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="skill-container">
+    <Title :title="t('skill.title')"/>
+    <Divider :color="'rgba(185,185,185,0.3)'"/>
+
+    <div v-if="Array.isArray(skillList)">
+      <div
+          v-for="(item, index) in skillList"
+          class="skill-list-item"
+          :key="index"
+      >
+        <div class="title">{{ item.title }}</div>
+        <div class="content">{{ item.content }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Title from "./Title.vue";
+import Divider from "./Divider.vue";
+import { ref } from "vue";
+import { useI18n } from 'vue-i18n'
+const { t, tm } = useI18n()
+
+const skillList = ref(tm("skill.list"))
+</script>
+
+<style lang="scss" scoped>
+.skill-container {
+  padding: 2vw 5vw;
+  & .skill-list-item {
+    padding: 0.7vw 1vw;
+    & .title {
+      @apply text-[1.3vw] font-bold;
+    }
+    & .content {
+      @apply text-[1.2vw] text-[#888888] whitespace-pre-wrap;
+    }
+  }
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <Info/>
+    <Skill/>
     <EducationalQualifications/>
   </div>
 </template>
 
 <script setup>
 import Info from "../components/info.vue";
+import Skill from "../components/Skill.vue";
 import EducationalQualifications from "../components/EducationalQualifications.vue";
 </script>
