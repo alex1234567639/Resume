658190975aeabd57ece70cfca6076f5b1e1ffea1
alex1234567639 <alex8603062000@gmail.com>
2025-03-09 15:44:28 +0800

    [feature]新增title組件

---
+++ src/components/Title.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="title-container">
+    <img src="/src/assets/img/item.webp" alt="">
+    {{ title }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+})
+</script>
+
+<style lang="scss" scoped>
+.title-container {
+  @apply flex items-center font-semibold;
+  font-size: 1.2vw;
+  & img {
+    margin: 0 0.2vw 0 0;
+    width: 3vw;
+  }
+}
+</style>
